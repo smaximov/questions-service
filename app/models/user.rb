167d5b1ec4ffcf9ef621e93163b1662b27a4e8de
smaximov <s.b.maximov@gmail.com>
2016-09-20 22:16:55 +1000

    Validate User's username and fullname

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable
+
+  validates :username, presence: true
+  validates :username, uniqueness: true
+  validates :username, format: /\A[\da-zA-Z._\-]+\z/, allow_nil: true
+  validates :username, length: { within: 3..20 }, allow_nil: true
+
+  validates :fullname, presence: true
+  validates :fullname, length: { within: 5..30 }, allow_nil: true
+
+  # Strip value and assign it to fullname.
+  def fullname=(value)
+    super(value.try(:strip))
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,66 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject { FactoryGirl.build(:user) }
+  let(:user) { FactoryGirl.build(:user) }
 
   it 'is valid with valid attributes' do
-    expect(subject).to be_valid
+    expect(user).to be_valid
+  end
+
+  describe '#username' do
+    it 'is required' do
+      user.username = nil
+      user.valid?
+      expect(user.errors[:username].size).to eq(1)
+    end
+
+    it 'is unique' do
+      FactoryGirl.create(:user, username: user.username)
+      user.valid?
+      expect(user.errors[:username].size).to eq(1)
+    end
+
+    it 'contains only valid letters' do
+      user.username = 'john doe'
+      user.valid?
+      expect(user.errors[:username].size).to eq(1)
+    end
+
+    it 'has minimum length of 3' do
+      user.username = 'jd'
+      user.valid?
+      expect(user.errors[:username].size).to eq(1)
+    end
+
+    it 'has maximum length of 20' do
+      user.username = 'a' * 21
+      user.valid?
+      expect(user.errors[:username].size).to eq(1)
+    end
+  end
+
+  describe '#fullname' do
+    it 'is required' do
+      user.fullname = nil
+      user.valid?
+      expect(user.errors[:fullname].size).to eq(1)
+    end
+
+    it 'has minimum length of 5' do
+      user.fullname = 'a' * 4
+      user.valid?
+      expect(user.errors[:fullname].size).to eq(1)
+    end
+
+    it 'has maximum length of 30' do
+      user.fullname = 'a' * 31
+      user.valid?
+      expect(user.errors[:fullname].size).to eq(1)
+    end
+
+    it 'is stripped of surrounded whitespace' do
+      user.fullname = " John Doe \n\t"
+      expect(user.fullname).to eq('John Doe')
+    end
   end
 end
