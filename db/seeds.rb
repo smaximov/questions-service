9188baa5b1dc3ff845db80a9f4149db99b4e6d98
smaximov <s.b.maximov@gmail.com>
2016-09-21 22:51:34 +1000

    Generate sample users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,23 @@
+# coding: utf-8
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+EXTRA_USERS = ENV.fetch('EXTRA_USERS') { 10 }.to_i
+
+# Create a new user, skipping email confirmation.
+def create_user(options = {})
+  id = options.delete(:id)
+
+  options[:username] ||= id ? "anonymous#{id}" : Faker::Internet.user_name
+  options[:email] ||= id ? "anonymous#{id}@example.com" : Faker::Internet.email
+  options[:fullname] ||= Faker::Name.name
+  options[:password] ||= Faker::Internet.password
+  options[:confirmed_at] ||= Time.zone.now
+
+  User.create!(options)
+end
+
+# Mandatory users
+create_user(username: 'ivanov', fullname: 'Иванов Иван', password: 'foobar')
+create_user(username: 'zoeva', fullname: 'Зоя Зоева', password: 'foobar')
+
+EXTRA_USERS.times { |n| create_user(id: n) }
