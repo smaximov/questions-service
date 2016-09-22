52c059b68f5756805a07b3e8c224fb60e2c6e792
smaximov <s.b.maximov@gmail.com>
2016-09-22 21:36:06 +1000

    Display sign up link on the home page unless user is signed in

--- spec/views/home/index.html.slim_spec.rb
+++ spec/views/home/index.html.slim_spec.rb
@@ -2,5 +2,19 @@
 require 'rails_helper'
 
 RSpec.describe 'home/index.html.slim', type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'when user is signed in' do
+    it 'displays no sign up links' do
+      allow(view).to receive(:user_signed_in?).and_return(true)
+      render
+      expect(rendered).not_to have_link(nil, href: new_user_registration_path)
+    end
+  end
+
+  context 'when user is not signed in' do
+    it 'displays the sign up link' do
+      allow(view).to receive(:user_signed_in?).and_return(false)
+      render
+      expect(rendered).to have_link(nil, href: new_user_registration_path, count: 1)
+    end
+  end
 end
