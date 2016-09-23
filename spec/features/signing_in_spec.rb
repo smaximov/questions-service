b599ab985184dc8990ca1a83cb3002e375f61b6e
smaximov <s.b.maximov@gmail.com>
2016-09-23 22:32:40 +1000

    Test signing in with either email or username as login

---
+++ spec/features/signing_in_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.feature 'Signing in' do
+  let(:user) { FactoryGirl.create(:confirmed_user) }
+
+  scenario 'Using username as login' do
+    visit new_user_session_path
+    within('#new_user') do
+      fill_in 'Login', with: user.username
+      fill_in 'Password', with: user.password
+      click_button 'Log in'
+    end
+    expect(page).to have_css('.flash-messages .alert-notice', text: 'successfully')
+    expect(page).to have_current_path(root_path)
+  end
+
+  scenario 'Using email as login' do
+    visit new_user_session_path
+    within('#new_user') do
+      fill_in 'Login', with: user.email
+      fill_in 'Password', with: user.password
+      click_button 'Log in'
+    end
+    expect(page).to have_css('.flash-messages .alert-notice', text: 'successfully')
+    expect(page).to have_current_path(root_path)
+  end
+end
