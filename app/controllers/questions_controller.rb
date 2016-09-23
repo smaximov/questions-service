8acbcb2e73f2d6cb5425c66b1c1b00bb4e0b2fbc
smaximov <s.b.maximov@gmail.com>
2016-09-23 15:59:12 +1000

    Create and display questions

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+class QuestionsController < ApplicationController
+  before_action :authenticate_user!, only: %i(new create)
+
+  def new
+    @question = current_user.questions.build
+  end
+
+  def create
+    @question = current_user.questions.build(question_params)
+    if @question.save
+      flash.now[:success] = 'New question created!'
+      redirect_to @question
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :body)
+  end
+end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
 # frozen_string_literal: true
 module ApplicationHelper
+  # Render error messages for resource, if any.
+  def error_messages_for(resource)
+    return if resource.errors.blank?
+
+    render 'shared/error_messages', errors: resource.errors.full_messages
+  end
 end
