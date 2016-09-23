f7cae1ea6e98c0b1b4f8c5ed703f2faf0e0b5502
smaximov <s.b.maximov@gmail.com>
2016-09-23 20:40:57 +1000

    Sign up link in page header unless user is signed in

--- spec/views/layouts/_header.slim_spec.rb
+++ spec/views/layouts/_header.slim_spec.rb
@@ -9,6 +9,10 @@ RSpec.describe 'layouts/_header.slim', type: :view do
       render
     end
 
+    it 'displays the sign up link' do
+      expect(rendered).to have_link(nil, href: new_user_registration_path, count: 1)
+    end
+
     it 'displays the sign in link' do
       expect(rendered).to have_link(nil, href: new_user_session_path, count: 1)
     end
@@ -26,6 +30,10 @@ RSpec.describe 'layouts/_header.slim', type: :view do
       allow(view).to receive(:user_signed_in?).and_return(true)
       allow(view).to receive(:current_user).and_return(user)
       render
+    end
+
+    it 'displays no sign up links' do
+      expect(rendered).not_to have_link(nil, href: new_user_registration_path)
     end
 
     it 'displays the sign out link' do
