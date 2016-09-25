3e61092250fa30c750b8aabec3f4121ec0f2b9a5
smaximov <s.b.maximov@gmail.com>
2016-09-25 20:20:19 +1000

    Generate sample questions

--- README.md
+++ README.md
@@ -4,3 +4,6 @@
 для вакансии от Daturum.
 
 [Демо](https://glacial-coast-40310.herokuapp.com/).
+
+Для демонстрации созданы пользователи `ivanov`, `zoeva`, `user-0`..`user-24` с
+паролем "foobar" (см. [db/seeds.rb](db/seeds.rb)).
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,37 @@
 # coding: utf-8
 # frozen_string_literal: true
 
-EXTRA_USERS = ENV.fetch('EXTRA_USERS') { 10 }.to_i
-
-# Create a new user, skipping email confirmation.
-def create_user(options = {})
-  id = options.delete(:id)
-
-  options[:username] ||= id ? "anonymous#{id}" : Faker::Internet.user_name
-  options[:email] ||= id ? "anonymous#{id}@example.com" : Faker::Internet.email
-  options[:fullname] ||= Faker::Name.name
-  options[:password] ||= Faker::Internet.password
-  options[:confirmed_at] ||= Time.zone.now
-
-  User.create!(options)
-end
+# EXTRA_USERS to create in addition to ivanov and zoeva
+EXTRA_USERS = ENV.fetch('EXTRA_USERS') { 25 }.to_i
+# zero to QUESTIONS_PER_USERS questions to create for each user
+QUESTIONS_PER_USER = ENV.fetch('QUESTIONS_PER_USER') { 5 }.to_i
 
 # Mandatory users
-create_user(username: 'ivanov', fullname: 'Иванов Иван', password: 'foobar')
-create_user(username: 'zoeva', fullname: 'Зоя Зоева', password: 'foobar')
+ivanov = User.create!(username: 'ivanov', fullname: 'Иванов Иван',
+                      email: Faker::Internet.email, password: 'foobar',
+                      created_at: 1.year.ago, confirmed_at: 1.year.ago + 5.minutes)
 
-EXTRA_USERS.times { |n| create_user(id: n) }
+question = <<QUESTION
+Живу на ст. метро Октябрьская. Хочу найти ближайшую библиотеку,
+но яндекс говорит, что надо ехать на Китай-город. Это слишком
+далеко для меня. Кто знает, как быть?
+QUESTION
+ivanov.questions.create!(title: 'Как пройти в библиотеку?', question: question,
+                         created_at: 1.day.ago)
+
+User.create!(username: 'zoeva', fullname: 'Зоя Зоева',
+             email: Faker::Internet.email, password: 'foobar',
+             created_at: 1.year.ago, confirmed_at: 1.year.ago + 5.minutes)
+
+# Additional users
+EXTRA_USERS.times do |n|
+  user = User.create!(username: "user-#{n}", email: "user-#{n}@example.com",
+                      fullname: Faker::Name.name, password: 'foobar',
+                      created_at: 1.year.ago, confirmed_at: 1.year.ago + 5.minutes)
+
+  rand(0..QUESTIONS_PER_USER).times do
+    user.questions.create!(title: Faker::Hipster.sentence.truncate(50),
+                           question: Faker::Hipster.paragraph(5),
+                           created_at: Faker::Time.between(user.created_at, Time.current))
+  end
+end
