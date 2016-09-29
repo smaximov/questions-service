b547c84fb3b365652297b8cbeebf0c96894327a0
smaximov <s.b.maximov@gmail.com>
2016-09-29 14:17:03 +1000

    No more RoutingErrors when posting /users or /questions then reloading

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,27 @@
 # frozen_string_literal: true
+
+class UrlHelpersRedirector
+  def self.method_missing(method, *args, **kwargs) # rubocop:disable Style/MethodMissing
+    new(method, args, kwargs)
+  end
+
+  def initialize(url_helper, args, kwargs)
+    @url_helper = url_helper
+    @args = args
+    @kwargs = kwargs
+  end
+
+  def call(params, _request)
+    url_helpers.public_send(@url_helper, *@args, **@kwargs.merge(locale: params[:locale]))
+  end
+
+  private
+
+  def url_helpers
+    Rails.application.routes.url_helpers
+  end
+end
+
 Rails.application.routes.draw do
   def available_locales
     @available_locales ||= /#{I18n.available_locales.join('|')}/
@@ -10,5 +33,8 @@ Rails.application.routes.draw do
     devise_for :users
 
     resources :questions, only: %i(new create show)
+
+    get '/users', to: redirect(UrlHelpersRedirector.new_user_registration_path)
+    get '/questions', to: redirect(UrlHelpersRedirector.new_question_path)
   end
 end
