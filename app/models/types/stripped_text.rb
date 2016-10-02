015efaf8cf4b0ee154026de452e053bbc4d5341f
smaximov <s.b.maximov@gmail.com>
2016-10-02 13:43:24 +1000

    Refactor StrippedText#cast_value

--- app/models/types/stripped_text.rb
+++ app/models/types/stripped_text.rb
@@ -3,7 +3,6 @@
 # Text stripped of surrounding whitespace.
 class StrippedText < ActiveRecord::Type::Text
   def cast_value(value)
-    value = value.to_s unless value.respond_to?(:strip)
-    value.strip
+    value.to_s.strip
   end
 end
