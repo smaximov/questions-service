68d67a527e7353a21f5501f599f0c7503faef925
smaximov <s.b.maximov@gmail.com>
2016-10-14 00:03:10 +1000

    Set mail sender to the Heroku app host

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -13,7 +13,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'noreply@questions.com'
+  config.mailer_sender = 'noreply@glacial-coast-40310.herokuapp.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
